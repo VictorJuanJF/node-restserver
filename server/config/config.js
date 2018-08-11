922ae629d573ca8a2c6df0d4a8c486352dfabb0b
Victor Juan <vj.jimenez96@gmail.com>
2018-08-10 21:55:01 -0500

    Added enviroment var

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:sed4cfv5@ds139781.mlab.com:39781/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
